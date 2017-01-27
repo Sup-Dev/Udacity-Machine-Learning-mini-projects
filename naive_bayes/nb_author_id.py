bc181d77d596a07402bbb9bf5ba824a9edeb4e26
Sup-Dev <sourabh.dev90@gmail.com>
2017-01-27 18:42:55 -0500

    feat: completed the email classifier based project on naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+pred = clf.predict(features_test)
+
+print("Accuracy via accuracy_score: ", accuracy_score(labels_test, pred))
+print("Accuracy via score method: ", clf.score(features_test, labels_test))
 #########################################################
 
 
