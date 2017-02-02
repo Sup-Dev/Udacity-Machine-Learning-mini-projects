75fbd895ced245aaa09ea99c37186dd8794dde34
Sup-Dev <sourabh.dev90@gmail.com>
2017-02-02 17:51:05 -0500

    feat: completed outlier removal mini project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,13 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    number_of_points = len(predictions)/10
 
-    
+    for i in range(len(predictions)):
+        cleaned_data.append((ages[i], net_worths[i], abs(predictions[i] - net_worths[i])))
+
+    cleaned_data.sort(key=lambda t: t[2], reverse=True)
+    cleaned_data = cleaned_data[number_of_points:]
+
     return cleaned_data
 
